10feaf35d31d5811a37b5ffb277ef5914017a064
chanathipgeist <88129306+chanathipgeist@users.noreply.github.com>
2022-09-10 13:49:33 +0700

    Update getBMIMeaning.js

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,17 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  console.log('Ploy Geist')
+  return weight/Math.pow(height,2)
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let bmi=calculateBMI(weight, height)
+  if(bmi>25.0){
+    return Overweight
+  }else if(bmi>=18.5 && bmi<=24.9){
+  return Normal weight
+}else{
+ return Underweight
 }
+}
+
 module.exports = getBMIMeaning
